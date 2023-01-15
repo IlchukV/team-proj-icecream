a99b35ff157e15b35dadb070319d78d1018f1646
braharalex <uljador@gmail.com>
2023-01-15 12:49:31 +0200

    final changes

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@
 })();
 
 // Modal
-(() => {
+/* (() => {
     const refs = {
         openModalBtn: document.querySelector("[data-modal-open]"),
         closeModalBtn: document.querySelector("[data-modal-close]"),
@@ -41,7 +41,7 @@
     function toggleModal() {
         refs.modal.classList.toggle("is-hidden");
     }
-})();
+})(); */
 
 // Modal +
 
@@ -76,8 +76,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
          /* После того как нашли нужное модальное окно, добавим классы
             подложке и окну чтобы показать их. */
-         modalElem.classList.add('active');
-         overlay.classList.add('active');
+         modalElem.classList.remove('is-hidden');
+         overlay.classList.remove('is-hidden');
       }); // end click
 
    }); // end foreach
@@ -88,30 +88,11 @@ document.addEventListener('DOMContentLoaded', function() {
       item.addEventListener('click', function(e) {
          var parentModal = this.closest('.modal');
 
-         parentModal.classList.remove('active');
-         overlay.classList.remove('active');
+         parentModal.classList.add('is-hidden');
+         overlay.classList.add('is-hidden');
       });
 
    }); // end foreach
-
-
-    document.body.addEventListener('keyup', function (e) {
-        var key = e.keyCode;
-
-        if (key == 27) {
-
-            document.querySelector('.modal.active').classList.remove('active');
-            document.querySelector('.overlay').classList.remove('active');
-        };
-    }, false);
-
-
-    overlay.addEventListener('click', function() {
-        document.querySelector('.modal.active').classList.remove('active');
-        this.classList.remove('active');
-    });
-
-
 
 
 }); // end ready
